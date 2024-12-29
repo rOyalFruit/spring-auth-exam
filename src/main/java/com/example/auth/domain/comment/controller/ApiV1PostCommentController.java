827e3c25e02b25abf7ea87892a575456259af8ad
rOyalFruit <ekdeon@gmail.com>
2024-12-30 07:22:39 +0900

    1번 방법 - 별도의 함수를 만들고 그 함수에 @Transactional 어노테이션을 붙여서 해결, self 사용 필수

--- src/main/java/com/example/auth/domain/comment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/example/auth/domain/comment/controller/ApiV1PostCommentController.java
@@ -12,6 +12,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +24,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ApiV1PostCommentController {
 
+    @Autowired
+    @Lazy
+    private ApiV1PostCommentController self;
     private final Rq rq;
     private final PostService postService;
 
@@ -62,22 +67,29 @@ public class ApiV1PostCommentController {
     ) {
     }
     @PostMapping
-    @Transactional
     public RsData<Void> writeItem(
             @PathVariable long postId,
             @RequestBody @Valid PostCommentWriteReqBody reqBody
+    ) {
+        PostComment postComment = self._writeItem(postId, reqBody);
+        return new RsData<>(
+                "201-1",
+                "%d번 댓글이 작성되었습니다.".formatted(postComment.getId())
+        );
+    }
+
+    @Transactional
+    public PostComment _writeItem(
+            long postId,
+            PostCommentWriteReqBody reqBody
     ) {
         Member actor = rq.checkAuthentication();
         Post post = postService.findById(postId).orElseThrow(
                 () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
         );
-        PostComment postComment = post.addComment(
+        return post.addComment(
                 actor,
                 reqBody.content
-        );
-        return new RsData<>(
-                "201-1",
-                "%d번 댓글이 작성되었습니다.".formatted(postComment.getId())
         );
     }
 }
